6c88a5573e7a6d43f95bd017cb3c0a5fe27ebfa5
Andrew Raffensperger <raffy@me.com>
2023-07-15 19:00:45 -0700

    minor

--- lib/src/test/java/adraffy/ens/normalize/Tests.java
+++ lib/src/test/java/adraffy/ens/normalize/Tests.java
@@ -5,7 +5,6 @@ import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 import org.json.JSONArray;
